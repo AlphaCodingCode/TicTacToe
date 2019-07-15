6187e9b56e7d3d1372ad35617a10242ddaee6efe
AlphaCodingcode <dilshan@alphacoding.com>
2019-07-15 17:42:00 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,4 +1,12 @@
 // Any global variables can be defined up here
+let CircleTurn = true;
+
+const VALUEOPEN = 0;
+const VALUEO = 1;
+const VALUEX = 2;
+let row1 = [0, 0, 0]
+let row2 = [0, 0, 0]
+let row3 = [0, 0, 0]
 
 
 /*
@@ -14,4 +22,99 @@ function setup() {
 function draw() {
     // Update
     // Render
+    drawBoard();
+}
+
+
+function mouseClicked() {
+    // figure out which coordinate row the place the entity
+    var row = 0;
+    var column = 0;
+
+    // figure out row
+    if (mouseY <= height / 3) {
+        row = 0;
+    } else if (mouseY <= 2* height / 3) {
+        row = 1;
+    } else {
+        row = 2;
+    }
+
+    // figure out column
+    if (mouseX <= width / 3) {
+        column = 0;
+    } else if (mouseX <= 2 * width / 3) {
+        column = 1;
+    } else {
+        column = 2;
+    }
+
+    // check if position is free
+    if (!isPositionFree(column, row)) {
+        // can't place a symbol here, exit
+        return;
+    }
+
+    // draw Circle or X on position
+    if (CircleTurn) {
+        drawCircle(column * width / 3 + width / 6, row * height / 3 + height / 6);
+        placeSymbolPosition(column, row, VALUEO);
+    } else {
+        drawX(column * width / 3, row * height / 3);
+        placeSymbolPosition(column, row, VALUEX);
+    }
+    CircleTurn = !CircleTurn;
+}
+
+
+/* Functions responsible for drawing board and elements */
+
+function drawCircle(x, y) {
+    noFill();
+    strokeWeight(8);
+    if (width > height) {
+        ellipse(x, y, height / 3 - 16, height / 3 - 16);
+    } else {
+        ellipse(x, y, width / 3 - 16, width / 3 - 16);
+    }
+}
+
+function drawX(x, y) {
+    strokeWeight(8);
+    line(x, y, x + width / 3, y + height / 3);
+    line(x, y + height / 3, x + width / 3, y);
+}
+
+function drawBoard() {
+    strokeWeight(5);
+    line(width / 3, 0, width / 3, height);
+    line(2 * width / 3, 0, 2 * width / 3, height);
+    line(0, height / 3, width, height / 3);
+    line(0, 2 * height / 3, width, 2 * height / 3);
+}
+
+/* Functions responsible for checking board state */
+
+function isPositionFree(column, row) {
+    if (row == 0) {
+        // check row1 array
+        return row1[column] == VALUEOPEN;
+    } else if (row == 1) {
+        // check row2 array
+        return row2[column] == VALUEOPEN;
+    } else {
+        // check row3 array
+        return row3[column] == VALUEOPEN;
+    }
+}
+
+
+function placeSymbolPosition(column, row, symbol) {
+    if (row == 0) {
+        row1[column] = symbol;
+    } else if (row == 1) {
+        row2[column] = symbol;
+    } else {
+        row3[column] = symbol;
+    }
 }
